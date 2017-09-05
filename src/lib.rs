2a40f4fe31278a5e30f0e5918df9546d5435d5ba
Kyle Wilcox <wilcox32ahs@gmail.com>
2017-09-05 16:27:26 -0400

    init:

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod problem_1;
+pub mod problem_2;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        print!("Hello world!");
+        // panic!("Shouldn't reach here!");
+        // unimplemented!();
+    }
+}
